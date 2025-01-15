0cbe36b41ff6821d8bde9a2056e4cff17cb215a5
조아 <parkjoah@naver.com>
2025-01-15 19:46:27 +0900

    feat: todos map

--- src/Todo.js
+++ src/Todo.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Todo() {
   const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState([]); // 기본값 : 빈 array
   const onChange = (event) => {
     setToDo(event.target.value);
     console.log(toDo);
@@ -12,10 +12,13 @@ function Todo() {
     if (toDo === "") {
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           value={toDo}
@@ -25,6 +28,12 @@ function Todo() {
         ></input>
         <button>Add ToDo</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
