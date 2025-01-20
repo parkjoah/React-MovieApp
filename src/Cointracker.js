af6556c2e14f4f8f447e375c1020168dd80435a3
조아 <parkjoah@naver.com>
2025-01-20 23:29:39 +0700

    feat: Cointracker - loading, coins

--- src/Cointracker.js
+++ src/Cointracker.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 
 function Cointracker() {
-  const [loading, serLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers");
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
-      <h1>The coins!</h1>
-      {loading ? <strong>loading</strong> : null}
+      <h1>The coins! ({coins.length})</h1>
+      {loading ? <strong>loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}):${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
