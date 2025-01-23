c182b130c6e5a001706a1968d89e5faa82c9917b
조아 <parkjoah@naver.com>
2025-01-23 19:01:19 +0900

    feat: Moviecomponent

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Moviecomponent from "./components/Moviecomponent";
 
 function Movie() {
   const [loading, setLoading] = useState(true);
@@ -23,16 +24,13 @@ function Movie() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
+            <Moviecomponent
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
