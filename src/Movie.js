3901ab80899c73330bec667ad64ff0a18ac433fd
조아 <parkjoah@naver.com>
2025-01-24 16:09:15 +0900

    feat: BrowserRouter , link

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function Movie() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default Movie;
