fa5e88c86cff3c8bddd7659e050a10d0808322e0
조아 <parkjoah@naver.com>
2025-01-25 21:56:56 +0900

    feat: useParams, link by id

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ function Movie() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/hello" element={<h1>Hello</h1>} />
       </Routes>
     </Router>
