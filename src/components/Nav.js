432e5b07a84e70c2042f0ed13a66d4c1f5dbb798
조아 <parkjoah@naver.com>
2025-02-23 22:33:39 +0900

    feat : 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 function Navigation() {
-  return <div className={styles.nav}></div>;
+  return (
+    <div className={styles.nav}>
+      <Link to={`/`} className={styles.toHome}>
+        MOVIE KEKE
+      </Link>
+    </div>
+  );
 }
 
 export default Navigation;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   const { id } = useParams();
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div className={styles.detailWrap}>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <>
+          <div className={styles.info}>
+            <img src={movie.medium_cover_image} alt={movie.title}></img>
+            <h3>{movie.title}</h3>
+          </div>
+          <p> genres : {movie.genres}</p>
+          <p> runtime : {movie.runtime} min</p>
+          <p> language : {movie.language} </p>
+          <p>rating : {movie.rating}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
